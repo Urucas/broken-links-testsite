b40d66698ee881cad9f71b67885a0ac4223cf044
Bruno Alassia <vruno.alassia@gmail.com>
2014-12-17 03:38:31 -0300

    add more colors to uncolors

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ io.on('connection', function(socket){
 		var spawn = require('child_process').spawn;
 		var args = []; 
 				args.push("-s");
-				args.push(params.site);
+				args.push("kuesty.com");
 				args.push("-404");
-				args.push(params.e404);
+				args.push("kuesty.com/error/e404");
 
 		var	child = spawn("broken-links", args);
 				child.on("close", function(code, signal){
--- static/js/app.js
+++ static/js/app.js
@@ -16,10 +16,29 @@ $(document).ready(function(){
 });
 
 function uncolor(stdout){
-	stdout = stdout.replace(/\[37m/g, '<span class="white">');
-	stdout = stdout.replace(/\[33m/g, '<span class="yellow">');
-	stdout = stdout.replace(/\[36m/g, '<span class="cyan">');
+
+	/* require('colors') colors array
+	 * black: [30, 39],
+	 * red: [31, 39],
+	 * green: [32, 39],
+   * yellow: [33, 39],
+   * blue: [34, 39],
+   * magenta: [35, 39],
+   * cyan: [36, 39],
+   * white: [37, 39],
+   * gray: [90, 39],
+   * grey: [90, 39],
+	 */
+
+	stdout = stdout.replace(/\[30m/g, '<span class="black">');
 	stdout = stdout.replace(/\[31m/g, '<span class="red">');
+	stdout = stdout.replace(/\[32m/g, '<span class="green">');
+	stdout = stdout.replace(/\[33m/g, '<span class="yellow">');
+	stdout = stdout.replace(/\[34m/g, '<span class="blue">');
+	stdout = stdout.replace(/\[35m/g, '<span class="magenta">');
+	stdout = stdout.replace(/\[36m/g, '<span class="cyan">');
+	stdout = stdout.replace(/\[37m/g, '<span class="white">');
+	stdout = stdout.replace(/\[90m/g, '<span class="gray">');
 	stdout = stdout.replace(/\[39m/g, '</span>');
 	stdout = "test@broken-links: " + stdout;
 	return stdout;
